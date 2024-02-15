b68ae89fe3006a8b423158fdfa4acf194d001251
xdeejm97 <d.niesmialowski@gmail.com>
2024-02-15 10:58:28 +0100

    Few changes in controller

--- src/main/java/com/atipera/task/controller/GithubController.java
+++ src/main/java/com/atipera/task/controller/GithubController.java
@@ -2,14 +2,14 @@ package com.atipera.task.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GithubController {
 
     @GetMapping("/")
-    public String get(){
+    public String get(@PathVariable("repoName") String repoName, @PathVariable("login") String login){
         return "Hello";
     }
 }
-git
